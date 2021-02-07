89b28e8dbe8af4fe21ccf855eb8cdcaabeb1254d
sotirr <sotirr@gmail.com>
2021-02-07 13:07:10 +0500

    reverse

--- abakpress/abakpress/urls.py
+++ abakpress/abakpress/urls.py
@@ -1,4 +1,4 @@
-"""test_site URL Configuration
+"""abakpress URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from nestedpages.views import StaticPageView
 
 urlpatterns = [
+    re_path(r'^(?P<url>.*)$', StaticPageView.as_view(), name='page'),
     path('admin/', admin.site.urls),
 ]
---
+++ abakpress/nestedpages/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+
+class Page(models.Model):
+    url = models.CharField(_('URL'), max_length=100)
+    parent_url = models.CharField(_('Parrent URL'),  max_length=100, null=True)
+    name = models.CharField(_('name'), max_length=100, db_index=True)
+    head = models.CharField(_('head'), max_length=200)
+    content = models.TextField(_('content'), blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('page', kwargs={'url': self.url})
